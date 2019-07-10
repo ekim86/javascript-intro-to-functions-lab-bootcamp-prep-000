2344ca77f0e67d4180ce0b365af4ee91ea3cc693
eunice kim <oonis86@gmail.com>
2019-07-10 02:57:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function sayHiToGrandma(string) {
   return ("I can't hear you!");
   else if (string==string.toUpperCase);
   return ("YES INDEED!");
-  else if (string==string.)
+  else if (string==string"I love you, too.")
   end;
 }
