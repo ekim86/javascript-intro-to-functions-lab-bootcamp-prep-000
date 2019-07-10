e2e506a03ac25b58c5b828d8c03d7e59b7d7266e
eunice kim <oonis86@gmail.com>
2019-07-10 03:02:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function logWhisper(string) {
   console.log('hello'); }
 function sayHiToGrandma(string) {
   if (string==string.toLowerCase)
-  return ("I can\'t hear you!");
+  return ("I can't hear you!");
   else if (string==string.toUpperCase)
   return ("YES INDEED!");
   else if (string=="I love you, Grandma.")
